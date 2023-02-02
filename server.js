3933e2987fff23680a020371f3c2e984bcecbdae
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2023-02-02 18:17:11 -0500

    Finished up async function createImage. Miscellaneous other changes.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // import modules/packages/dependencies
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 8001;
-require("dotenv").config();
+require('dotenv').config();
 
-// import createImage
-// import createImage from "./server/createImage";
+// load custom module
+const { createImage } = require('./server/createImage');
 
 const app = express();
 app.use(express.json());
@@ -17,11 +17,11 @@ app.use(cors());
 app.get('/', async (req, res, next) => {
   try {
     // get absolute path of index.html
-    const root = path.join(__dirname, "index.html");
+    const root = path.join(__dirname, 'index.html');
     // respond by sending index.html file
     res.status(200).sendFile(root);
   } catch (error) {
-    console.log("***** Error! Something undesired happened: *****\n", error);
+    console.log('***** Error! Something undesired happened: *****\n', error);
     res.status(400).json(error);
   }
 });
@@ -30,7 +30,7 @@ app.get('/transcription', async (req, res) => {
     try {
       const response = await axios.post(
         // use account token to get a temp user token
-        "https://api.assemblyai.com/v2/realtime/token",
+        'https://api.assemblyai.com/v2/realtime/token',
         // can set a TTL timer in seconds.
         { expires_in: 3600 },
         // AssemblyAI API Key goes here; saved as environment variable for privacy/security
@@ -43,31 +43,20 @@ app.get('/transcription', async (req, res) => {
   
       res.json(data);
     } catch (error) {
-      console.log("***** Error! Something undesired happened: *****\n", error);
+      console.log('***** Error! Undesired event in /transcription endpoint: *****\n', error);
       res.status(400).json(error);
     }
 });
 
-app.post("/createImage", async (req, res) => {
+app.post('/createImage', async (req, res) => {
   try {
-    const { prompt } = req.body;
-    res.json({ prompt: prompt })
-    // const response = await axios.post(
-    //   // use account token to get a temp user token
-    //   "https://api.assemblyai.com/v2/realtime/token",
-    //   // can set a TTL timer in seconds.
-    //   { expires_in: 3600 },
-    //   // AssemblyAI API Key goes here; saved as environment variable for privacy/security
-    //   { headers: { authorization: process.env.ASSEMBLYAI_API_KEY } }
-    // );
-    // // destructure data property from response
-    // const { data } = response;
-    // // create new environment variable to store temporary authentication token
-    // process.env.TEMP_TOKEN = data.token;
-
-    // res.json(data);
+    // console.log(req.body);
+    const { prompt, n, size } = req.body;
+    // call custom function createImage()
+    const returnedData = await createImage(prompt, n, size);
+    res.json({ returnedData })
   } catch (error) {
-    console.log("***** Error! Something undesired happened: *****\n", error);
+    console.log('***** Error! Undesired event in /createImage endpoint: *****\n', error);
     res.status(400).json(error);
   }
 });
--- server/createImage.js
+++ server/createImage.js
@@ -10,23 +10,23 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // define async function createImage
-const createImage = async (prompt, number, size) => {
+const createImage = async (prompt, n, size) => {
   try {
     // make HTTP POST request to endpoint @ https://api.openai.com/v1/images/generations
     const response = await openai.createImage({
-      prompt: prompt,
-      n: number,
-      size: size,
+      prompt,
+      n,
+      size
     });
     // log to console
     console.log(response.data.data[0].url + "\n\n");
     // return data
-    return data;
+    return response.data.data;
   } catch (error) {
-    console.error('Something unexpected or undesired happened.\n', error);
+    console.error('***** Error in async function createImage: *****\n', error);
   }
 };
 
 // createImage('A dog riding a surfboard in turbulent water, as a pencil drawing.', 1, '1024x1024');
-
-export default createImage;
+// export default createImage;
+module.exports = { createImage };
