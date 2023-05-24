eccf218146e7c059103626388991a601fcdb85c4
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2023-05-24 14:24:38 -0400

    Experimenting with idea of integrating graphql-upload to handle image data storage question. Changed Apollo Server instance configuration to prevent cross-site request forgery (CSRF) and amended header object for every request sent to GraphQL API server.

--- server/server.js
+++ server/server.js
@@ -1,36 +1,42 @@
 // import connection to mongodb database
-const db = require('./config/connection');
+const db = require("./config/connection");
 // import modules/packages/dependencies
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
 // import authentication middleware
-const { authMiddleware } = require('./utils/authorization')
+const { authMiddleware } = require("./utils/authorization");
 const PORT = process.env.PORT || 8001;
 
 // import schema type definitions and resolvers
-const { typeDefs, resolvers } = require('./src');
+const { typeDefs, resolvers } = require("./src");
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 // create new instance of an Apollo Server, passing in type definitions and resolvers
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   // every request performed will have an authentication check done
   // the updated request object will be passed to the resolvers as the context
-  context: authMiddleware
+  context: authMiddleware,
+  csrfPrevention: true,
 });
 
-const routes = require('./routes');
-
+const routes = require("./routes");
 const app = express();
+// to use graphql-uploads, we must attach non-empty apollo-require-preflight header along with all requests;
+// this is required because we enabled cross-site request forgery (CSRF) prevention in Apollo Server instance
+app.use(function (req, res, next) {
+  req.headers["apollo-require-preflight"] = "true";
+  next();
+});
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
-console.log(path.join(__dirname));
+//console.log(path.join(__dirname));
 // app.use(express.static(path.join(__dirname, 'public')));
-// enable CORS for all origins  
+// enable CORS for all origins
 app.use(cors());
 
 app.use(routes);
